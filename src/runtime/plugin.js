34c4df9e09a8cb4935d60d61cfa138dbd0cb10e0
krftcz <lukas@krft.cz>
2024-02-22 16:10:13 +0100

    fix useCookie

--- src/runtime/plugin.js
+++ src/runtime/plugin.js
@@ -1,4 +1,4 @@
-import {defineNuxtPlugin, addRouteMiddleware} from '#app';
+import {defineNuxtPlugin, addRouteMiddleware, useCookie} from '#app';
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook('app:created', () => {
